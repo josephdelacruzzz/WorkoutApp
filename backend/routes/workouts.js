6d9525227224e6ba359e5c885a584d59332e4c85
josephdelacruzzz <jdelacruz1631@gmail.com>
2025-02-23 01:29:06 -0800

    Made the frontend with react to work with the backend

---
+++ backend/routes/workouts.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const {
+    createWorkout,
+    getWorkouts,
+    getWorkout,
+    deleteWorkout,
+    updateWorkout
+} = require('../controllers/workoutController')
+
+const router = express.Router()
+
+//GET all workouts
+router.get('/', getWorkouts)
+
+//GET a single workout
+router.get('/:id', getWorkout)
+
+//POST a single workout
+router.post('/', createWorkout)
+
+//DELETE a single workout
+router.delete('/:id', deleteWorkout)
+
+//UPDATE a single workout
+router.patch('/:id', updateWorkout)
+
+module.exports = router
+
